944a15a52334f842d83f9f33d3ba302746d94f69
MajoGT2015 <morguapos2015@gmail.com>
2025-06-29 17:23:29 -0300

    cambios

--- proyecto_tp_integrador_grupo9/src/redux/Store.js
+++ proyecto_tp_integrador_grupo9/src/redux/Store.js
@@ -1,6 +1,6 @@
 
 import { configureStore } from '@reduxjs/toolkit';
-import productsReducer from './productsSlice';
+import productsReducer from './ProductsSlice';
 
 export const store = configureStore({
   reducer: {
